5c6db0f5174588e985183c803e6045c41a5d9594
Virtuode <pathansehjad41@gmail.com>
2026-02-25 10:14:16 +0530

    Third commit of Product fetch by Id

--- src/module/product/product.routes.ts
+++ src/module/product/product.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createProduct, getAllProduct } from "./product.controller.js";
+import {
+  createProduct,
+  getAllProduct,
+  getProductById,
+} from "./product.controller.js";
 
 const router = Router();
 
 router.post("/", createProduct);
 router.get("/", getAllProduct);
+router.get("/:id", getProductById);
 
 export default router;
